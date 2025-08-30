ca07e5a602d0ac9cab372d5737c1cd4cdef57d61
ammar258540 <smandalibas@gmail.com>
2025-08-30 08:15:29 +0800

    Add files via upload
    Initial commit - Sistem Absensi Digital

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'absensi-guru-v1.0';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+];
+
+// Install Service Worker
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Cache opened');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch Event
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+// Update Service Worker
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
